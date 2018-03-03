b0f7dfa81d85e887cb16fec5186953344a08ba99
Francis Santerre <francis.santerre@gmail.com>
2018-03-03 10:58:22 -0500

    Add new controller action to get version

--- app/code/local/Beeweb/Wordpressproducts/controllers/PluginController.php
+++ app/code/local/Beeweb/Wordpressproducts/controllers/PluginController.php
@@ -3,6 +3,8 @@
 class Beeweb_Wordpressproducts_PluginController
     extends Mage_Core_Controller_Front_Action
 {
+    const PLUGIN_VERSION = '1.0.3';
+
     public function verifyAction()
     {
         $code = $this->getRequest()->getParam('code');
@@ -24,6 +26,18 @@ class Beeweb_Wordpressproducts_PluginController
         $defaultStore = Mage::app()->getDefaultStoreView();
         $jsonData['default_store'] = $defaultStore->getCode();
         $jsonData = json_encode($jsonData);
+
+        $this->getResponse()->setHeader('Content-type', 'application/json');
+        $this->getResponse()->setBody($jsonData);
+    }
+
+    public function versionAction()
+    {
+        $jsonData = json_encode(
+            array(
+                'version' => self::PLUGIN_VERSION
+            )
+        );
 
         $this->getResponse()->setHeader('Content-type', 'application/json');
         $this->getResponse()->setBody($jsonData);
